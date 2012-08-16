6a5873da9cea24d96688a4b8eb9ed9783b2ffbea
Jack <jacoby.kevin@live.fr>
2012-08-17 01:40:03 +0200

    add Handler of DeleteCardEvent on the main presenter

--- src/be/betty/gwtp/client/presenters/MainPresenter.java
+++ src/be/betty/gwtp/client/presenters/MainPresenter.java
@@ -21,6 +21,8 @@ import be.betty.gwtp.client.event.AddNotifEvent;
 import be.betty.gwtp.client.event.AddNotifEvent.AddNotifHandler;
 import be.betty.gwtp.client.event.BoardViewChangedEvent;
 import be.betty.gwtp.client.event.CardFilterEvent;
+import be.betty.gwtp.client.event.DeleteCardEvent;
+import be.betty.gwtp.client.event.DeleteCardEvent.DeleteCardHandler;
 import be.betty.gwtp.client.event.DropCardEvent;
 import be.betty.gwtp.client.event.InstancesModifiedEvent;
 import be.betty.gwtp.client.event.SetViewEvent;
@@ -184,6 +186,12 @@ public class MainPresenter extends
 		}
 	};
 	
+	private DeleteCardHandler deleteCardHandler = new DeleteCardHandler() {
+		@Override public void onDeleteCard(DeleteCardEvent event) {
+			System.out.println("dans le main l'id du carton:: "+event.getCardId());
+		}
+	};
+	
 	private CardFilterHandler filterHandler = new CardFilterHandler() {
 
 		@Override public void onCardFilter(CardFilterEvent event) {
@@ -337,6 +345,10 @@ public class MainPresenter extends
 		
 		registerHandler(getEventBus().addHandler(
 				ShowPlacedCardEvent.getType(), showPlacedHandler));
+		
+		registerHandler(getEventBus().addHandler(
+				DeleteCardEvent.getType(), deleteCardHandler));
+		
 		
 	}
 
