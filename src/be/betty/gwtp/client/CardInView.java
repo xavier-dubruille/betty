8e8755c6189d44e1398a9fec1a21b2ab087aa9b1
Xavier <dubruille.xavier@gmail.com>
2012-08-19 20:51:49 +0200

    Working group view

--- src/be/betty/gwtp/client/CardInView.java
+++ src/be/betty/gwtp/client/CardInView.java
@@ -24,17 +24,20 @@ public class CardInView {
 		//System.out.println("Group   = num "+Storage_access.getGroupCard(card));
 		//System.out.println("Teacher= "+Storage_access.getTeacher(Integer.parseInt(Storage_access.getTeacherCard(card) ) ));
 		//System.out.println("Group= "+Storage_access.getGroup(Integer.parseInt(Storage_access.getGroupCard(card))));
+		
 		int c2 = combo_viewChoice2.getSelectedIndex();
 		//System.out.println("   Selected choice = "+c2);
 		switch (combo_viewChoice1.getSelectedIndex()){
-		case 0:
+		case 0: // teacher
 			if (Storage_access.getTeacherIdCard(card) == c2) return true;
 
 			break;
-		case 1:
+		case 1: // room
 			break;
-		case 2:
-			//if (Storage_access.getGroupIdCard(card) == c2) return true;
+		case 2: // group
+			for (int i:Storage_access.getGroupsIdCard(card))
+				if ( i== c2) return true;
+			
 			break;
 		}
 		//System.out.println("==>no");
