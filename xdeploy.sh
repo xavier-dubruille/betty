fb6010964615f7521f5f2b1c37314e53663c62a7
Xavier <dubruille.xavier@gmail.com>
2012-08-19 15:09:45 +0200

    My upload script..

--- xdeploy.sh
+++ xdeploy.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 
-echo "*** Starting remove the (remote) old project"
-ssh root@betty.sytes.net "rm -rf /var/lib/tomcat6/webapps/$1"
-ssh root@betty.sytes.net "mkdir /var/lib/tomcat6/webapps/$1"
 
-echo "*** Done remove, starting copy local files"
+echo "*** Copy local files in /tmp"
 rm -fr /tmp/bett
 mkdir /tmp/bett
 cp -r war/* /tmp/bett/
+
+echo "*** Ok, you can go back to work ;) "
 rm -r /tmp/bett/WEB-INF/lib
 rm -r /tmp/bett/WEB-INF/deploy
 rm -r /tmp/bett/betty_gwtp/sc
 
+echo "*** Remove the (remote) old project"
+ssh root@betty.sytes.net "rm -rf /var/lib/tomcat6/webapps/betty/*"
+
 echo "*** Sending data .."
-scp -r /tmp/bett/* root@betty.sytes.net:/var/lib/tomcat6/webapps/$1
+scp -r /tmp/bett/* root@betty.sytes.net:/var/lib/tomcat6/webapps/betty/
 
 echo "*** Copying libraries in the remote war directory"
-ssh root@betty.sytes.net "cp -r /var/lib/tomcat6/webapps/all_libs /var/lib/tomcat6/webapps/$1/WEB-INF/lib"
-ssh root@betty.sytes.net "cp -r /var/lib/tomcat6/webapps/skins /var/lib/tomcat6/webapps/$1/betty_gwtp/sc"
+ssh root@betty.sytes.net "cp -r /var/lib/tomcat6/all_libs /var/lib/tomcat6/webapps/betty/WEB-INF/lib"
+ssh root@betty.sytes.net "cp -r /var/lib/tomcat6/skins /var/lib/tomcat6/webapps/betty/betty_gwtp/sc"
 
 
 echo "*** Let's restart tomcat.."
